14a63b4d9eda506c6a5b4478e7322e34ceeb7a99
DLeafGreen <66260675+saidlab@users.noreply.github.com>
2025-05-07 15:41:56 -0300

    Update test.lua

--- test.lua
+++ test.lua
@@ -85,7 +85,7 @@ else
 	print("[Webhook] Mensagem enviada com sucesso! Código de status:", response.StatusCode or response.statusCode)
 end
 
-queue_on_teleport(`_G.Webhook = {_G.Webhook} loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/saidlab/auto-rift/refs/heads/main/test.lua"))()`)
+queue_on_teleport("_G.Webhook = ".._G.Webhook.." loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/saidlab/auto-rift/refs/heads/main/test.lua'))()")
 
 task.wait(30)
 
@@ -111,6 +111,7 @@ while not found do
 		if result.nextPageCursor then
 			cursor = result.nextPageCursor
 		else
+			print('Parou :(")
 			break
 		end
 	else
